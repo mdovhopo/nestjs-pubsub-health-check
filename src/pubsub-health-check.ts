d4d9f7642027d0560ccb41504066f6ca7dcef948
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2021-04-14 00:16:24 +0300

    fix: exports

--- src/pubsub-health-check.ts
+++ src/pubsub-health-check.ts
@@ -1,4 +1,8 @@
 /* istanbul ignore file */
 
-export {} from './pubsub-health-check/pubsub-health-check.service';
-export {} from './pubsub-health-check/pubsub-health-check.module';
+export { PubSubHealthCheckService } from './pubsub-health-check/pubsub-health-check.service';
+export {
+  PubSubHealthCheckModule,
+  PubSubHealthCheckSettings,
+  PubSubHealthCheckModuleOptions,
+} from './pubsub-health-check/pubsub-health-check.module';
